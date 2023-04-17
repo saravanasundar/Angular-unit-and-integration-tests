ca664239797be99476bf810cdca6ff6c5822db60
saravanasundar <saravanasundar133@gmail.com>
2023-04-17 14:08:49 +0530

    Updated to angular 13 and made changes to run test files locally

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -3,17 +3,17 @@ import { HeroComponent } from './hero.component';
 
 describe('HeroComponent (Shallow tests)', () => {
   let fixture: ComponentFixture<HeroComponent>;
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [HeroComponent],
-  //   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent],
+    });
 
-  //   fixture = TestBed.createComponent(HeroComponent);
-  // });
+    fixture = TestBed.createComponent(HeroComponent);
+  });
 
-  // it('should have the correct hero', () => {
-  //   fixture.componentInstance.hero = { id: 1, name: 'Hero1', strength: 10 };
+  it('should have the correct hero', () => {
+    fixture.componentInstance.hero = { id: 1, name: 'Hero1', strength: 10 };
 
-  //   expect(fixture.componentInstance.hero.name).toEqual('Hero1');
-  // });
+    expect(fixture.componentInstance.hero.name).toEqual('Hero1');
+  });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,49 @@
-import './polyfills';
+// import './polyfills';
 
-import './test/jasmine-setup';
+// import './test/jasmine-setup';
 
-import 'jasmine-core/lib/jasmine-core/jasmine-html.js';
-import 'jasmine-core/lib/jasmine-core/boot.js';
+// import 'jasmine-core/lib/jasmine-core/jasmine-html.js';
+// import 'jasmine-core/lib/jasmine-core/boot.js';
 
-import './test.ts';
+// // import './test.ts';
+
+// import { AppModule } from './app/app.module';
+
+// import { platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+// platformBrowserDynamicTesting()
+//   .bootstrapModule(AppModule)
+//   .then((ref) => {
+//     // Ensure Angular destroys itself on hot reloads.
+//     if (window['ngRef']) {
+//       window['ngRef'].destroy();
+//     }
+//     window['ngRef'] = ref;
+
+//     // Otherise, log the boot error
+//   })
+//   .catch((err) => console.error(err));
+
+// (function bootstrap() {
+//   if (window['jasmineRef']) {
+//     location.reload();
+
+//     return;
+//   }
+
+//   window.onload(new Event('anything'));
+//   // window['jasmineRef'] = jasmine.getEnv();
+// })();
+
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-import { platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+if (environment.production) {
+  enableProdMode();
+}
 
-platformBrowserDynamicTesting()
-  .bootstrapModule(AppModule)
-  .then((ref) => {
-    // Ensure Angular destroys itself on hot reloads.
-    if (window['ngRef']) {
-      window['ngRef'].destroy();
-    }
-    window['ngRef'] = ref;
-
-    // Otherise, log the boot error
-  })
-  .catch((err) => console.error(err));
-
-(function bootstrap() {
-  if (window['jasmineRef']) {
-    location.reload();
-
-    return;
-  }
-
-  window.onload(new Event('anything'));
-  window['jasmineRef'] = jasmine.getEnv();
-})();
+platformBrowserDynamic().bootstrapModule(AppModule)
